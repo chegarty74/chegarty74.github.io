bd129a7be45da7fe2c579b1407f032f755f7cf11
chegarty74 <chegarty74@yahoo.com>
2017-11-29 15:51:11 -0600

    WEDNESDAY UPDATE
    Re-uploaded entire HWS file with latest mobile responsive changes.

---
+++ HWS/HWS/JS/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 400, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+    $("#menu-toggle").click(function () {
+        $("nav").slideToggle(500);
+    });
+
+    $(".navlinks").click(function () {
+        $("nav").slideUp(500);
+    });
+});
---
+++ HWS/HWS/JS/scrolltop.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    // When the user scrolls down 300px from the top of the document, show the button
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+
+
+
+
+
+});
